532a5812449c536c8e72ee221c827e3db6a122e4
Tanuki <tanuki.contact@gmail.com>
2024-11-15 14:59:12 +0100

    updates

--- client/src/App.vue
+++ client/src/App.vue
@@ -28,6 +28,10 @@ const query = gql`
             title {
               userPreferred
             }
+            type
+            status
+            format
+            siteUrl
             relations {
               edges {
                 relationType
@@ -39,6 +43,7 @@ const query = gql`
                   type
                   status
                   format
+                  siteUrl
                 }
               }
             }
@@ -96,40 +101,68 @@ async function loadData () {
 <template>
   <main>
     <button @click="loadData">Load</button>
+
+    <section>
+      <fieldset>
+        <legend>Relation</legend>
+        <div class="flex flex-wrap gap-2">
+          <div v-for="relation in MediaRelation" class="flex-shrink-0">
+            <input type="checkbox" name="relation" :id="'relation_' + relation" v-model="visibility.relation" :value="relation">
+            <label :for="'relation_' + relation">{{ relation }}</label>
+          </div>
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <legend>Format</legend>
+        <div class="flex flex-wrap gap-2">
+          <div v-for="format in MediaFormat" class="flex-shrink-0">
+            <input type="checkbox" name="format" :id="'format_' + format" v-model="visibility.format" :value="format">
+            <label :for="'format_' + format">{{ format }}</label>
+          </div>
+        </div>
+      </fieldset>
+      
+      <fieldset>
+        <legend>Status</legend>
+        <div class="flex flex-wrap gap-2">
+          <div v-for="status in MediaStatus" class="flex-shrink-0">
+            <input type="checkbox" name="status" :id="'status_' + status" v-model="visibility.status" :value="status">
+            <label :for="'status_' + status">{{ status }}</label>
+          </div>
+        </div>
+      </fieldset>
+    </section>
+
     <template v-if="mediaListCollection">
-      <section v-for="list in mediaListCollection.lists">
-        <table>
-          <thead>
-            <tr>
-              <th>{{ list!.name }}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="entry in list!.entries">
-              <td>
-                <table>
-                  <thead>
-                    <tr>
-                      <th>{{ entry!.media!.title!.userPreferred }} ({{ seenIds.includes(entry!.media!.id) ? 'VIEWED' : 'NOT VIEWED' }})</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr
-                      v-for="edge in entry!.media!.relations!.edges"
-                      :class="{'hidden': !isVisible(edge!) }">
-                      <td>{{ seenIds.includes(edge!.node!.id) ? 'VIEWED' : 'NOT VIEWED' }}</td>
-                      <td>{{ edge!.relationType }}</td>
-                      <td>{{ edge!.node!.title!.userPreferred }}</td>
-                      <td>{{ edge!.node!.type }}</td>
-                      <td>{{ edge!.node!.status }}</td>
-                      <td>{{ edge!.node!.format }}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <section v-for="list in mediaListCollection.lists" class="p-4">
+        <h2>{{ list!.name }}</h2>
+
+        <section class="space-y-4">
+          <section v-for="entry in list!.entries">
+            <div class="p-2 bg-gray-300">
+              <a
+                class="font-medium text-blue-500 hover:underline"
+                :href="entry?.media?.title?.userPreferred!"
+                target="_blank"
+              >{{ entry!.media!.title!.userPreferred }} ({{ seenIds.includes(entry!.media!.id) ? 'VIEWED' : 'NOT VIEWED' }})
+              </a>
+            </div>
+            <section>
+              <div
+                v-for="edge in entry!.media!.relations!.edges"
+                class="space-x-2"
+                :class="{'hidden': !isVisible(edge!) }">
+                <span v-if="!seenIds.includes(edge?.node?.id!)" class="px-2 text-sm font-medium text-white bg-red-500 rounded-full">Pas vu</span>
+                <span>{{ edge!.relationType }}</span>
+                <a class="font-medium text-blue-500" :href="edge?.node?.siteUrl!" target="_blank">{{ edge!.node!.title!.userPreferred }}</a>
+                <span>{{ edge!.node!.type }}</span>
+                <span>{{ edge!.node!.status }}</span>
+                <span>{{ edge!.node!.format }}</span>
+              </div>
+            </section>
+          </section>
+        </section>
       </section>
     </template>
   </main>
